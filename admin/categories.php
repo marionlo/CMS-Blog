f80829f9ba0d7325941438055e7d8488d728ccbd
Marion-Lotito <marion.lotito@gmail.com>
2021-07-15 11:25:57 +0200

    create html table to display posts in admin

--- admin/categories.php
+++ admin/categories.php
@@ -17,26 +17,7 @@
                         </h1>
                         <div class="col-xs-6">
                              <!-- Create Category Feature -->
-                            <?php 
-                            
-                            if(isset($_POST['submit'])) {
-                                $cat_title = $_POST['cat_title'];
-
-                                if($cat_title == "" || empty($cat_title)) {
-                                    echo "This field should not be empty";
-                                } else {
-                                    $query = "INSERT INTO categories(cat_title) ";
-                                    $query .= "VALUE('{$cat_title}') ";
-
-                                    $create_category_query = mysqli_query($connection, $query);
-
-                                    if(!$create_category_query) {
-                                        die('QUERY FAILED' . mysqli_error($connection));
-                                    }
-                                }
-                            }
-                            
-                            ?>
+                            <?php insert_categories();?>
                             <!-- Add Category Form -->
                             <form action="" method="post">
                                 <div class="form-group">
@@ -48,19 +29,19 @@
                                 </div>
                             </form>
 
-                            <?php 
+                            <?php // Update and include query
                         
                         if(isset($_GET['edit'])) {
                             $cat_id = $_GET['edit'];
                             include "includes/edit_categories.php";
                         }
                         
-                        ?>
+                             ?>
                         </div>
 
                     <div class="col-xs-6">
 
-                        <table class="table table-bordered">
+                        <table class="table table-bordered table-hover">
                             <thead>
                                 <tr>
                                     <th>Id</th>
@@ -68,36 +49,10 @@
                                 </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <!-- Find all Categories and Display Categories Feature -->
-                            <?php
-
-                            $query = "SELECT * FROM categories";
-                            $select_categories = mysqli_query($connection, $query);     
-                             while($row = mysqli_fetch_assoc($select_categories )) {
-                             $cat_title = $row['cat_title'];
-                             $cat_id = $row['cat_id'];
-                             echo "<tr>";
-                             echo "<td>{$cat_id}</td>";   
-                             echo "<td>{$cat_title}</td>";
-                             echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-                             echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-                             echo "</tr>";
-                    } ?>
-                             <?php 
-                             //Delete the category
-                             if(isset($_GET['delete'])) {
-                                 $the_cat_id = $_GET['delete'];
-                                 $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id} ";
-                                 $delete_query = mysqli_query($connection, $query);
-                                 header("Location: categories.php"); //This will refresh the page
-                             }
-                             
-                             
-                            ?>   
-
-                                     
-                                
+                            
+                            <?php findAllCategories() // Find all Categories and Display Categories Feature ?>
+                            <?php deleteCategories() //Delete the category ?>   
+             
                             </tbody>
                         </table>
                     </div>
---
+++ admin/posts.php
@@ -0,0 +1,57 @@
+<?php include "includes/admin_header.php" ?>
+<div id="wrapper">
+
+        <!-- Navigation -->
+    <?php include "includes/admin_navigation.php" ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                                Welcome to admin
+                                <small>Author name</small>
+                        </h1>
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Image</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <tr>
+                                <td>10</td>
+                                <td>John Doe</td>
+                                <td>Le titre de l'article</td>
+                                <td>Bootstrap</td>
+                                <td>Published</td>
+                                <td>Image</td>
+                                <td>Tags</td>
+                                <td>Comments</td>
+                                <td>Date</td>
+                            </tr>
+                        </tbody>
+                        </table>
+                        
+                    </div>
+                    </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+        <!-- /#page-wrapper -->
+
+    <?php include "includes/admin_footer.php" ?>
