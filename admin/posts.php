9030c5c4ccfbef1ee4c89bed0cbab0c1617084ed
Marion-Lotito <marion.lotito@gmail.com>
2021-07-15 11:33:18 +0200

    display post lists in admin

--- admin/posts.php
+++ admin/posts.php
@@ -30,18 +30,32 @@
                                 </tr>
                             </thead>
                             <tbody>
-                            <tr>
-                                <td>10</td>
-                                <td>John Doe</td>
-                                <td>Le titre de l'article</td>
-                                <td>Bootstrap</td>
-                                <td>Published</td>
-                                <td>Image</td>
-                                <td>Tags</td>
-                                <td>Comments</td>
-                                <td>Date</td>
-                            </tr>
-                        </tbody>
+                            <?php $query = "SELECT * FROM posts";
+                                $select_posts = mysqli_query($connection, $query);     
+                                while($row = mysqli_fetch_assoc($select_posts )) {
+                                $post_id = $row['post_id'];
+                                $post_author = $row['post_author'];
+                                $post_title = $row['post_title'];
+                                $post_category = $row['post_category_id'];
+                                $post_status = $row['post_status'];
+                                $post_image = $row['post_image'];
+                                $post_tags = $row['post_tags'];
+                                $post_comments = $row['post_comment_count'];
+                                $post_date = $row['post_date'];
+                                echo "<tr>";
+                                echo "<td>{$post_id}</td>";   
+                                echo "<td>{$post_author}</td>";
+                                echo "<td>{$post_title}</td>";
+                                echo "<td>{$post_category}</td>";
+                                echo "<td>{$post_status}</td>";
+                                echo "<td>{$post_image}</td>";
+                                echo "<td>{$post_tags}</td>";
+                                echo "<td>{$post_comments}</td>";
+                                echo "<td>{$post_date}</td>";
+                                echo "</tr>";
+                            } ?>
+                          
+                            </tbody>
                         </table>
                         
                     </div>
