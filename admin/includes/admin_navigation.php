a2da500d2ecf9d3805ea0fa31eaf25654c857c65
Marion-Lotito <marion.lotito@gmail.com>
2021-07-16 18:07:59 +0200

    create comments admin page and query for the data

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -58,7 +58,7 @@
                         </ul>
                     </li>
                     <li class="active">
-                        <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments</a>
+                        <a href="comments.php"><i class="fa fa-fw fa-file"></i> Comments</a>
                     </li>
                     <li>
                         <a href="javascript:;" data-toggle="collapse" data-target="#users_dropdown"><i class="fa fa-fw fa-arrows-v"></i> Users <i class="fa fa-fw fa-caret-down"></i></a>
---
+++ admin/includes/view_all_comments.php
@@ -0,0 +1,66 @@
+<table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Id</th>
+                                    <th>Author</th>
+                                    <th>Comment</th>
+                                    <th>Email</th>
+                                    <th>Status</th>
+                                    <th>In response to</th>
+                                    <th>Date</th>
+                                    <th>Approve</th>
+                                    <th>Unapprove</th>
+                                    <th>Delete</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php $query = "SELECT * FROM comments";
+                                $select_comments = mysqli_query($connection, $query);     
+                                while($row = mysqli_fetch_assoc($select_comments)) {
+                                $comment_id = $row['comment_id'];
+                                $comment_author = $row['comment_author'];
+                                $comment_post_id = $row['comment_post_id'];
+                                $comment_email = $row['comment_email'];
+                                $comment_date = $row['comment_date'];
+                                $comment_content = $row['comment_content'];
+                                $comment_status = $row['comment_status'];
+                                echo "<tr>";
+                                echo "<td>{$comment_id}</td>";   
+                                echo "<td>{$comment_author}</td>";
+                                echo "<td>{$comment_content}</td>";
+
+                                // $query = "SELECT * FROM categories WHERE cat_id = $post_category_id ";
+                                //         $select_categories_id = mysqli_query($connection, $query);     
+                                //          while($row = mysqli_fetch_assoc($select_categories_id )) {
+                                //          $cat_title = $row['cat_title'];
+                                //          $cat_id = $row['cat_id'];
+
+                                // echo "<td>{$cat_title}</td>";
+                                //          }
+
+
+
+                                echo "<td>{$comment_email}</td>";
+                                echo "<td>{$comment_status}</td>";
+                                echo "<td>{$comment_post_id}</td>";
+                                echo "<td>{$comment_date}</td>";
+                                echo "<td><a href='posts.php?source=edit_post&p_id={}'>Approve</a></td>"; 
+                                echo "<td><a href='posts.php?unapprove={}'>Unapprove</a></td>";  
+                                echo "<td><a href='posts.php?delete={}'>Delete</a></td>";  
+                                echo "</tr>";
+                            } ?>
+
+                            <?php 
+                            
+                            if(isset($_GET['delete'])) {
+                                $the_post_id = $_GET['delete'];
+                                $query = "DELETE FROM posts WHERE post_id = {$the_post_id} ";
+                                $delete_query = mysqli_query($connection, $query);
+                                header("Location: posts.php"); //This will refresh the page
+                            }
+                            
+                            
+                            ?>
+                          
+                            </tbody>
+</table>
