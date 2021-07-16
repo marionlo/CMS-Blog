916435c43ca4d149d0579ff6aa42e7dad731b651
Marion-Lotito <marion.lotito@gmail.com>
2021-07-16 18:35:46 +0200

    create the query to send the comment data to the DB

--- post.php
+++ post.php
@@ -48,7 +48,7 @@
                 <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
                 <hr>
                 <p><?php echo $post_content ?></p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                
 
                 <hr>
 
@@ -59,7 +59,18 @@
                     <?php 
                     
                     if(isset($_POST['create_comment'])) {
+                        $the_post_id = $_GET['p_id'];
+                        $comment_author = $_POST['comment_author'];
+                        $comment_email = $_POST['comment_email']; 
+                        $comment_content = $_POST['comment_content'];  
                         
+                        $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date) ";
+                        $query .="VALUES ($the_post_id, '$comment_author', '$comment_email', '$comment_content', 'unapproved', now()) ";
+
+                        $create_comment_query = mysqli_query($connection, $query);
+                        if(!$create_comment_query) {
+                            die("QUERY FAILED" . mysqli_error($connection));
+                        }
                     }
                     
                     ?>
@@ -78,7 +89,7 @@
                         </div>
                         <div class="form-group">
                           <label for="comment">Your comment</label>
-                            <textarea class="form-control" rows="3"></textarea>
+                            <textarea class="form-control" rows="3" name="comment_content"></textarea>
                         </div>
                         <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
                     </form>
