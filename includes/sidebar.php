bfbdc4380e0184290090def4a0d9557001a5b185
Marion-Lotito <marion.lotito@gmail.com>
2021-07-19 12:07:43 +0200

    add login block on the sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -17,6 +17,23 @@
     <!-- /.input-group -->
 </div>
 
+<!-- Login -->
+<div class="well">
+    <h4>Login</h4>
+    <form action="includes/login.php" method="post">
+        <div class="form-group">
+            <input type="text" class="form-control" name="username" placeholder="Enter Username">
+        </div>
+        <div class="input-group">
+            <input type="password" class="form-control" name="password" placeholder="Enter Password">
+            <span class="input-group-btn">
+                <button class="btn btn-primary" name="login" type="submit">Submit</button>
+            </span> 
+        </div>
+    </form><!-- /Login form -->
+    <!-- /.input-group -->
+</div>
+
 
 
 
