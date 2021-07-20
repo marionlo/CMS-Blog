6911cca80725fa61545cbdd9c182156d57c5ec80
Marion-Lotito <marion.lotito@gmail.com>
2021-07-20 11:51:47 +0200

    add dropdown option for add post page

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -61,9 +61,12 @@ if(isset($_POST['create_post'])) {
         <input type="text" class="form-control" name="post_author" />
       </div>
      
-      <div class="form-group">
-        <label for="post_status">Post Status</label>
-        <input type="text" class="form-control" name="post_status" />
+      <div class="form-group">  
+        <select name="post_status" id="">
+          <option value="draft">Post Status</option>
+          <option value="published">Publish</option>
+          <option value="draft">Draft</option>
+        </select>    
       </div>
      
       <div class="form-group">
