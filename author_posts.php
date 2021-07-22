f10e605c09db852877404a4324e366b3ff03f3b2
Marion-Lotito <marion.lotito@gmail.com>
2021-07-22 14:44:54 +0200

    add related posts to their authors

--- author_posts.php
+++ author_posts.php
@@ -1,7 +1,5 @@
 <?php include "includes/db.php";?>
 <?php include "includes/header.php";?>
-
-
     <!-- Navigation -->
     <?php include "includes/navigation.php"; ?>
 
@@ -14,18 +12,23 @@
             <div class="col-md-8">
 
             <?php 
+
+                        // Catch the id of the post
+            if(isset($_GET['p_id'])) {
+                $the_post_id = $_GET['p_id'];
+                $the_post_author = $_GET['author'];
+            }
+
             
-            $query = "SELECT * FROM posts WHERE post_status = 'published'";
+            $query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}' ";
             $select_all_posts_query = mysqli_query($connection, $query);
 
                     while($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                        $post_id = $row['post_id'];
                         $post_title = $row['post_title'];
                         $post_author = $row['post_author'];
                         $post_date = $row['post_date'];
                         $post_image = $row['post_image'];
-                        $post_content = substr($row['post_content'],0,150);
-                        $post_status = $row['post_status'];
+                        $post_content = $row['post_content'];
 
                         ?>
 
@@ -36,37 +39,34 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title ?></a>
+                    <a href="#"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="author_posts.php?author=<?php echo $post_author ?>&p_id=<?php echo $post_id ?>"><?php echo $post_author ?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
                 <hr>
-                <a href="post.php?p_id=<?php echo $post_id; ?>">
                 <img class="img-responsive" src="images/<?php echo $post_image ?>" alt="">
-                    </a>
                 <hr>
-                <p><?php echo $post_content ?>...</p>
-                <a class="btn btn-primary" href="post.php?p_id=<?php echo $post_id; ?>">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                <p><?php echo $post_content ?></p>
+                
 
-                <hr>
+               <hr>
 
-                    <?php } if (mysqli_num_rows($select_all_posts_query)==0) {
-                            echo "<h1 class='text-center'>There is no post published yet</h1>";
-                            // Displays a message if there is no post published yet
-                        } ?>
 
+                    <?php } ?>
+                
+                
             </div>
 
 
 
             <!-- Blog Sidebar Widgets Column -->
-        <?php include "includes/sidebar.php" ?>
+            <?php include "includes/sidebar.php" ?>
 
         </div>
         <!-- /.row -->
 
         <hr>
    <!-- Footer -->
-     <?php include "includes/footer.php"; ?>
+   <?php include "includes/footer.php"; ?>
