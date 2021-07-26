f276a5bfccc3327daf410181e05b9e3b28a00254
Marion-Lotito <marion.lotito@gmail.com>
2021-07-26 16:36:23 +0200

    refactor add and update categories with placement

--- admin/functions.php
+++ admin/functions.php
@@ -45,15 +45,15 @@ function insert_categories() {
         if($cat_title == "" || empty($cat_title)) {
             echo "This field should not be empty";
         } else {
-            $query = "INSERT INTO categories(cat_title) ";
-            $query .= "VALUE('{$cat_title}') ";
+            $stmt = mysqli_prepare($connection, "INSERT INTO categories(cat_title) VALUES(?) ");
+            mysqli_stmt_bind_param($stmt, "s", $cat_title);
+            mysqli_stmt_execute($stmt);
 
-            $create_category_query = mysqli_query($connection, $query);
-
-            if(!$create_category_query) {
+            if(!$stmt) {
                 die('QUERY FAILED' . mysqli_error($connection));
             }
         }
+        mysqli_stmt_close($stmt);
     }
 }
 
--- admin/includes/edit_categories.php
+++ admin/includes/edit_categories.php
@@ -17,13 +17,12 @@
 
                                    <?php //Update the category into the DB
                                     if(isset($_POST['update_category'])) {
-                                        $the_cat_title = escape($_POST['cat_title']);
-                                        $query = "UPDATE categories SET cat_title = '{$the_cat_title}' WHERE cat_id = {$cat_id} ";
-                                        $update_query = mysqli_query($connection, $query);
-                                        if(!$update_query) {
-                                            die('QUERY FAILED' . mysqli_error($connection));
-                                        }
-                                        
+                                        $cat_title = escape($_POST['cat_title']);
+                                        $stmt = mysqli_prepare($connection, "UPDATE categories SET cat_title = ? WHERE cat_id = ? ");
+                                        mysqli_stmt_bind_param($stmt, "si", $cat_title, $cat_id);
+                                        mysqli_stmt_execute($stmt);
+                                        mysqli_stmt_close($stmt);
+                                        redirect("categories.php");              
                                     }
                                     
                                    ?>
