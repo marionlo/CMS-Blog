079617b9dfa7b750b60f86a08b75aa1d5376422b
Marion-Lotito <marion.lotito@gmail.com>
2021-07-27 20:06:50 +0200

    remove username and pw phpmailer

--- classes/config.php
+++ classes/config.php
@@ -3,8 +3,8 @@
 class Config {
     const SMTP_HOST = 'smtp.mailtrap.io';
     const SMTP_PORT = 2525;
-    const SMTP_USER = '7da414d0be9826';
-    const SMTP_PASSWORD = '80a321f2eae173';
+    const SMTP_USER = 'enter_your_phpmailer_username';
+    const SMTP_PASSWORD = 'enter_your_phpmailer_password';
 }
 
 
