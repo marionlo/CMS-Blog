8a1b44b013fb42b8a8813e3d6e8a59236bd7561d
Marion-Lotito <marion.lotito@gmail.com>
2021-07-29 15:11:12 +0200

    refactor view all comments page

--- admin/functions.php
+++ admin/functions.php
@@ -204,6 +204,7 @@ function findAllCategories() {
 
 }
 
+
 function findAllUserCategories() {
     global $connection;
     $query = "SELECT * FROM categories WHERE user_id=".loggedInUserId()."";
@@ -229,6 +230,90 @@ function deleteCategories() {
         $delete_query = mysqli_query($connection, $query);
         header("Location: categories.php"); //This will refresh the page
     }
+}
+
+function findAllComments() {
+    global $connection;
+    $query = "SELECT * FROM comments";
+    $select_comments = mysqli_query($connection, $query);     
+    while($row = mysqli_fetch_assoc($select_comments)) {
+    $comment_id = $row['comment_id'];
+    $comment_author = $row['comment_author'];
+    $comment_post_id = $row['comment_post_id'];
+    $comment_email = $row['comment_email'];
+    $comment_date = $row['comment_date'];
+    $comment_content = $row['comment_content'];
+    $comment_status = $row['comment_status'];
+    echo "<tr>";
+    echo "<td>{$comment_id}</td>";   
+    echo "<td>{$comment_author}</td>";
+    echo "<td>{$comment_content}</td>";
+    echo "<td>{$comment_email}</td>";
+    echo "<td>{$comment_status}</td>";
+
+    // Add the post title for each comment
+    $query = "SELECT * FROM posts WHERE post_id = $comment_post_id ";
+    $select_post_id = mysqli_query($connection, $query);     
+    while($row = mysqli_fetch_assoc($select_post_id)) {
+    $post_title = $row['post_title'];
+    $post_id = $row['post_id'];
+    echo "<td><a href='../post.php?p_id={$post_id}'>{$post_title}</a></td>";
+    }
+    echo "<td>{$comment_date}</td>";
+    echo "<td><a href='comments.php?approve={$comment_id}' class='btn btn-success'>Approve</a></td>"; 
+    echo "<td><a href='comments.php?unapprove={$comment_id}' class='btn btn-warning'>Unapprove</a></td>";  
+    echo "<td><a href='comments.php?delete={$comment_id}' class='btn btn-danger'>Delete</a></td>";  
+    echo "</tr>";
+}
+}
+
+function deleteComment() {
+    global $connection;
+    if(isset($_GET['delete'])) {
+        // Prevent people from deleting when they are not logged in
+           if(isset($_SESSION['user_role'])) {
+           if($_SESSION['user_role'] == 'admin') {
+       $the_comment_id = mysqli_real_escape_string($connection, $_GET['delete']);
+       $query = "DELETE FROM comments WHERE comment_id = {$the_comment_id} ";
+       $delete_query = mysqli_query($connection, $query);
+       $query2 = "UPDATE posts SET post_comment_count = post_comment_count - 1 ";
+       $query2 .= "WHERE post_id = $comment_post_id ";
+       $update_query = mysqli_query($connection,$query2);
+       header("Location: comments.php"); //This will refresh the page
+   }
+}
+}
+}
+
+
+function approveComment() {
+    global $connection;
+    if(isset($_GET['approve'])) {
+        // Prevent people from approving when they are not logged in
+           if(isset($_SESSION['user_role'])) {
+           if($_SESSION['user_role'] == 'admin') {
+       $the_comment_id = mysqli_real_escape_string($connection, $_GET['approve']);
+       $query = "UPDATE comments SET comment_status = 'approved' WHERE comment_id = {$the_comment_id} ";
+       $unapprove_query = mysqli_query($connection, $query);
+       header("Location: comments.php"); //This will refresh the page
+    }
+    }
+}
+}
+
+function unapproveComment() {
+    global $connection;
+    if(isset($_GET['unapprove'])) {
+        // Prevent people from unapproving when they are not logged in
+           if(isset($_SESSION['user_role'])) {
+           if($_SESSION['user_role'] == 'admin') {
+       $the_comment_id = mysqli_real_escape_string($connection, $_GET['unapprove']);
+       $query = "UPDATE comments SET comment_status = 'unapproved' WHERE comment_id = {$the_comment_id} ";
+       $unapprove_query = mysqli_query($connection, $query);
+       header("Location: comments.php"); //This will refresh the page
+    }
+    }
+}
 }
 
 
